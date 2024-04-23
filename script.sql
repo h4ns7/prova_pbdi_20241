913a57dd109d2bc333f65c4f638bbcae9677417e
Diego Müller <muller97_@outlook.com>
2024-04-23 10:37:02 -0300

    feat(cursor-unbound): cálculo de preço médio por país

--- script.sql
+++ script.sql
@@ -1,10 +1,58 @@
-CREATE TABLE winemag130k(
+
+-- Criando a tabela para armazenar os dados
+CREATE TABLE dados_vinhos (
 	cod_top_wine SERIAL PRIMARY KEY,
-	rank INT,
-	country VARCHAR (200),
-	description VARCHAR(2000),
- 	points NUMERIC(10,2),
-	price NUMERIC (10,2)
+	RANK INTEGER,
+    country VARCHAR(100),
+    description TEXT,
+    points INTEGER,
+    price NUMERIC
 );
+
+
+DO $$
+DECLARE
+    -- Declaração do cursor
+    cur_nomes_pais REFCURSOR;
+    -- Variáveis para armazenar os valores do cursor
+    v_country VARCHAR(100);
+    v_preco_medio NUMERIC(10,2);
+    -- Variável para o nome da tabela
+    v_nome_tabela VARCHAR(200) := 'dados_vinhos';
+BEGIN 
+    -- Abertura do cursor
+    OPEN cur_nomes_pais FOR EXECUTE 
+        format(
+            '
+            SELECT country, SUM(price) / COUNT(*) AS preco_medio
+            FROM %I
+            GROUP BY country
+            ORDER BY preco_medio DESC; -- Ordenar por preços maiores
+            ',
+            v_nome_tabela
+        );
+        
+    -- Loop para recuperar os resultados do cursor
+    LOOP
+        FETCH cur_nomes_pais INTO v_country, v_preco_medio;
+        -- Sair do loop quando não houver mais resultados
+        EXIT WHEN NOT FOUND;
+        -- Exibir os resultados
+        RAISE NOTICE 'País: %, Preço Médio: %', v_country, v_preco_medio;
+    END LOOP;
+    
+    -- Fechar o cursor
+    CLOSE cur_nomes_pais;
+END $$;
+
+
+
+
+
+
+
+
+
+
 
 
